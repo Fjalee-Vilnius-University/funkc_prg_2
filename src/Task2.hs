ee9ec22a2d12038456ad3b479648c9a5c7037352
Fjalee <cepRytis@gmail.com>
2020-10-17 18:05:45 +0300

    parseInt()

--- src/Task2.hs
+++ src/Task2.hs
@@ -1,2 +1,19 @@
 module Task2 where
+import Data.List as L
+import Data.Char as C
 import Task2Message
+
+--Int -> String -> Either String JsonLikeValue
+parse :: String
+parse = message
+
+parseInt :: String -> (Int, String)
+parseInt ('i':t) = 
+    let
+        prefix = L.takeWhile C.isDigit t
+        postfix = L.drop (length prefix) t
+    in
+            case postfix of
+                ('e':r) -> (read prefix, r)
+                _ -> error "Invalid Integer"
+parseInt _ = error "Not integer"
