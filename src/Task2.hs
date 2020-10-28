99a02cd783c2446c7fc16f328a26b66418b189b7
Fjalee <cepRytis@gmail.com>
2020-10-28 18:44:31 +0200

    parseString func to return int where error occured

--- src/Task2.hs
+++ src/Task2.hs
@@ -3,104 +3,120 @@ import Data.List as L
 import Data.Char as C
 import Task2Message
 
-parse :: Int -> String -> Either String JsonLikeValue
-parse size str = 
-    case parseJLMap str of
-        Left a -> Left a
-        Right (b, "") -> Right b
-        Right (b, _) -> Left "Some values are outside of map"
+-- parse :: Int -> String -> Either String JsonLikeValue
+-- parse size str = 
+--     case parseJLMap str of
+--         Left a -> Left a
+--         Right (b, "") -> Right b
+--         Right (b, _) -> Left "Some values are outside of map"
 
-parseJLMap :: String -> Either String (JsonLikeValue, String)
-parseJLMap ('d':t) = 
-    case parseAllMapedJLValues t of
-        Left a -> Left a
-        Right (mapBody, rest) -> Right (mapBody, rest)
-parseJLMap _ = Left "Error, map has to start with a 'd'"
+-- parseJLMap :: (String, Int) -> Either String (JsonLikeValue, String)
+-- parseJLMap (('d':t), errPos) = 
+--     case parseAllMapedJLValues t of
+--         Left a -> Left a
+--         Right (mapBody, rest) -> Right (mapBody, rest)
+-- parseJLMap (_, errPos) = Left "Error, map has to start with a 'd'"
 
-parseAllMapedJLValues :: String -> Either String (JsonLikeValue, String)
-parseAllMapedJLValues ('e':t) = Right (JLMap [], t)
-parseAllMapedJLValues str =
-    case parseMapedJLValue str of
-        Left a -> Left a
-        Right ((key, value), rest) ->
-            case parseAllMapedJLValues rest of
-                Left a -> Left a
-                Right (JLMap acc, rest1) -> Right $ (JLMap ([(key, value)] ++ acc), rest1)
-parseAllMapedJLValues [] = Left "Error, JLMap has to end with an 'e'"
+-- parseAllMapedJLValues :: (String, Int) -> Either String (JsonLikeValue, String)
+-- parseAllMapedJLValues (('e':t), errPos) = Right (JLMap [], t)
+-- parseAllMapedJLValues (str, errPos) =
+--     case parseMapedJLValue str of
+--         Left a -> Left a
+--         Right ((key, value), rest) ->
+--             case parseAllMapedJLValues rest of
+--                 Left a -> Left a
+--                 Right (JLMap acc, rest1) -> Right $ (JLMap ([(key, value)] ++ acc), rest1)
+-- parseAllMapedJLValues ([], errPos) = Left "Error, JLMap has to end with an 'e'"
 
-parseMapedJLValue :: String -> Either String ((String, JsonLikeValue), String)
-parseMapedJLValue str = 
-    case parseString str of
-        Left a -> Left a
-        Right (key, rest) ->
-            case parseJLValue rest of
-                Left a -> Left a
-                Right (value, rest') -> Right ((key, value), rest')
-parseMapedJLValue [] = Left "Empty maped JLValue"
+-- parseMapedJLValue :: (String, Int) -> Either String ((String, JsonLikeValue), String)
+-- parseMapedJLValue (str, errPos) = 
+--     case parseString str of
+--         Left a -> Left a
+--         Right (key, rest) ->
+--             case parseJLValue rest of
+--                 Left a -> Left a
+--                 Right (value, rest') -> Right ((key, value), rest')
+-- parseMapedJLValue ([], errPos) = Left "Empty maped JLValue"
 
-parseJLValue :: [Char] -> Either String (JsonLikeValue, String)
-parseJLValue ('d':t) =
-    case parseJLMap('d':t) of
-        Left a -> Left a
-        Right (a, b) -> Right (a, b)
-parseJLValue ('l':t) = 
-    case parseJLArray ('l':t) of
-        Left a -> Left a
-        Right (a, b) -> Right (a, b)
-parseJLValue ('i':t) = 
-    case parseJLInt ('i':t) of
-        Left a -> Left a
-        Right (a, b) -> Right (a, b)
-parseJLValue (h:t) = 
-    if (C.isDigit h)
-    then 
-        case parseJLString (h:t) of
-            Left a -> Left a
-            Right (a, b) -> Right (a, b)
-    else Left "Error, JsonLikeValue has to start with a 'd' or a 'l' or an 'i' or a digit"
-parseJLValue [] = Left "Empty JLValue"
+-- parseJLValue :: (String, Int) -> Either String (JsonLikeValue, String)
+-- parseJLValue (('d':t), errPos) =
+--     case parseJLMap('d':t) of
+--         Left a -> Left a
+--         Right (a, b) -> Right (a, b)
+-- parseJLValue (('l':t), errPos) = 
+--     case parseJLArray ('l':t) of
+--         Left a -> Left a
+--         Right (a, b) -> Right (a, b)
+-- parseJLValue (('i':t), errPos) = 
+--     case parseJLInt ('i':t) of
+--         Left a -> Left a
+--         Right (a, b) -> Right (a, b)
+-- parseJLValue ((h:t), errPos) = 
+--     if (C.isDigit h)
+--     then 
+--         case parseJLString (h:t) of
+--             Left a -> Left a
+--             Right (a, b) -> Right (a, b)
+--     else Left "Error, JsonLikeValue has to start with a 'd' or a 'l' or an 'i' or a digit"
+-- parseJLValue ([], errPos) = Left "Empty JLValue"
 
-parseJLArray :: String -> Either String (JsonLikeValue, String)
-parseJLArray ('l':t) = 
-    case parseJLIntOrString t of
-        Left a -> Left a
-        Right (value, (fstCh : rest)) ->
-            case fstCh of
-                'e' -> Right (JLArray [value], rest)
-                _ -> Left "Error, one element list has to end with an 'e' after first element in the array"
-parseJLArray _ = Left "Error, list has to start with an 'l'"
-parseJLArray [] = Left "Empty Array"
+-- parseJLArray :: (String, Int) -> Either String (JsonLikeValue, String)
+-- parseJLArray (('l':t), errPos) = 
+--     case parseJLIntOrString t of
+--         Left a -> Left a
+--         Right (value, (fstCh : rest)) ->
+--             case fstCh of
+--                 'e' -> Right (JLArray [value], rest)
+--                 _ -> Left "Error, one element list has to end with an 'e' after first element in the array"
+-- parseJLArray (_, errPos) = Left "Error, list has to start with an 'l'"
+-- parseJLArray ([], errPos) = Left "Empty Array"
 
-parseJLIntOrString :: String -> Either String (JsonLikeValue, String)
-parseJLIntOrString ('i':t) = 
-    case parseJLInt ('i':t) of
-        Left a -> Left a
-        Right (a, b) -> Right (a, b)
-parseJLIntOrString (h:t) =
-    if C.isDigit h
-    then case parseJLString (h:t) of
-        Left a -> Left a
-        Right (a, b) -> Right (a, b)
-    else Left "Error, Value is nether an Int or a String"
-parseJLIntOrString [] = Left "Empty Int or String"
+-- parseJLIntOrString :: (String, Int) -> Either String (JsonLikeValue, String)
+-- parseJLIntOrString (('i':t), errPos) = 
+--     case parseJLInt ('i':t) of
+--         Left a -> Left a
+--         Right (a, b) -> Right (a, b)
+-- parseJLIntOrString ((h:t), errPos) =
+--     if C.isDigit h
+--     then case parseJLString (h:t) of
+--         Left a -> Left a
+--         Right (a, b) -> Right (a, b)
+--     else Left "Error, Value is nether an Int or a String"
+-- parseJLIntOrString ([], errPos) = Left "Empty Int or String"
 
-parseJLInt :: String -> Either String (JsonLikeValue, String)
-parseJLInt ('i':t) = 
-        case C.isDigit $ head t of
-            False -> Left "Error, Integer has 0 digits"
-            True ->
-                    let
-                        prefix = L.takeWhile C.isDigit t
-                        postfix = L.drop (length prefix) t
-                    in
-                        case postfix of
-                            ('e':r) -> Right (JLInt (read prefix), r)
-                            _ -> Left "Error, Integer has to end with an 'e'"
-parseJLInt _ = Left "Error, Integer has to start with an 'i'"
-parseJLInt [] = Left "Empty Int"
+-- parseJLInt :: (String, Int) -> Either String (JsonLikeValue, String)
+-- parseJLInt (('i':t), errPos) = 
+--         case C.isDigit $ head t of
+--             False -> Left "Error, Integer has 0 digits"
+--             True ->
+--                     let
+--                         prefix = L.takeWhile C.isDigit t
+--                         postfix = L.drop (length prefix) t
+--                     in
+--                         case postfix of
+--                             ('e':r) -> Right (JLInt (read prefix), r)
+--                             _ -> Left "Error, Integer has to end with an 'e'"
+-- parseJLInt _ = Left "Error, Integer has to start with an 'i'"
+-- parseJLInt ([], errPos) = Left "Empty Int"
 
-parseJLString :: String -> Either String (JsonLikeValue, String)
-parseJLString str =
+-- parseJLString :: (String, Int) -> Either String (JsonLikeValue, String)
+-- parseJLString (str, errPos) =
+--     let
+--         strLen = if C.isDigit $ head str
+--             then L.takeWhile C.isDigit str
+--             else "not declared"
+--         postfix = L.drop (length strLen) str
+--     in
+--         case strLen of 
+--             "not declared" -> Left "Error, Length of the string was not declared"
+--             _ ->
+--                 case postfix of
+--                 (':':r) -> Right (JLString $ L.take (read strLen) r, L.drop (read strLen) r)
+--                 _ -> Left "Error, Invalid string"
+-- parseJLString ([], errPos) = Left "Empty String"
+
+parseString :: (String, Int) -> Either String (String, String, Int)
+parseString (str, errPos) =
     let
         strLen = if C.isDigit $ head str
             then L.takeWhile C.isDigit str
@@ -108,25 +124,13 @@ parseJLString str =
         postfix = L.drop (length strLen) str
     in
         case strLen of 
-            "not declared" -> Left "Error, Length of the string was not declared"
+            "not declared" -> Left ("Error around character " ++ show errPos ++ ", Length of the string was not declared")
             _ ->
                 case postfix of
-                (':':r) -> Right (JLString $ L.take (read strLen) r, L.drop (read strLen) r)
-                _ -> Left "Error, Invalid string"
-parseJLString [] = Left "Empty String"
+                (':':r) -> Right (L.take (read strLen) r, L.drop (read strLen) r, (errPos + lenDiff str (L.drop (read strLen) r)))
+                _ -> Left ("Error around character " ++ show errPos ++ ", Invalid string")
+parseString ([], errPos) = Left ("Error around character " ++ show errPos ++ ", Empty String")
+-- (errPos + length strLen + 1 + read strLen)
 
-parseString :: String -> Either String (String, String)
-parseString str =
-    let
-        strLen = if C.isDigit $ head str
-            then L.takeWhile C.isDigit str
-            else "not declared"
-        postfix = L.drop (length strLen) str
-    in
-        case strLen of 
-            "not declared" -> Left "Error, Length of the string was not declared"
-            _ ->
-                case postfix of
-                (':':r) -> Right (L.take (read strLen) r, L.drop (read strLen) r)
-                _ -> Left "Error, Invalid string"
-parseString [] = Left "Empty String"
+lenDiff :: String -> String -> Int
+lenDiff str1 str2 = (length str1) - (length str2)
--- src/Task2Message.hs
+++ src/Task2Message.hs
@@ -2,13 +2,13 @@ module Task2Message
 where
 
 -- ┌           ┐
--- │ O X X O O │
--- │ X X ? O O │
+-- │ O O O X ? │
+-- │ O X O O X │
 -- │ X X O O X │
--- │ X O X O X │
--- │ X X O O O │
+-- │ O X O X O │
+-- │ X O X X X │
 -- └           ┘
--- seed: -4586556274488441162
+-- seed: 5312648285961206292
 -- encoding: Ben
 -- list entry: ARR
 -- convert to: LIL
@@ -20,15 +20,15 @@ size :: Int
 size = 5
 
 message :: String
-message = "d4:prevd4:prevd4:lastd2:xsli3ee2:vsl1:Oe2:ysli3eee4:prevd4:prevd4:prevd4:lastd2:xsli4ee2:vsl1:Xe2:ysli3eee4:prevd4:prevd4:prevd4:prevd4:lastd2:xsli1ee2:vsl1:Xe2:ysli0eee4:prevd4:prevd4:prevd4:lastd2:xsli0ee2:vsl1:Oe2:ysli0eee4:prevd4:prevd4:prevd\"4:lastd2:xsli1ee2:vsl1:Xe2:ysli1eee4:prevd4:lastd2:xsli2ee2:vsl1:Oe2:ysli2eee4:prevd4:prevd4:prevd4:prevd4:lastd2:xsli2ee2:vsl1:Oe2:ysli4eee4:prevd4:prevd4:lastd2:xsli3ee2:vsl1:Oe2:ysli1eee4:prevd4:prevd4:lastd2:xsli2ee2:vsl1:Oe2:ysli1eee4:prevd4:lastd2:xsli2ee2:vsl1:Xe2:ysli1eeeee4:lastd2:xsli0ee2:vsl1:Xe2:ysli3eeeee4:lastd2:xsli1ee2:vsl1:Xe2:ysli4eeeee4:lastd2:xsli1ee2:vsl1:Xe2:ysli2eeee4:lastd2:xsli4ee2:vsl1:Oe2:ysli1eeee4:lastd2:xsli0ee2:vsl1:Xe2:ysli4eeeeee4:lastd2:xsli4ee2:vsl1:Oe2:ysli4eeee4:lastd2:xsli2ee2:vsl1:Xe2:ysli0eeeee4:lastd2:xsli2ee2:vsl1:Xe2:ysli3eeee4:lastd2:xsli3ee2:vsl1:Oe2:ysli2eeeee4:lastd2:xsli1ee2:vsl1:Oe2:ysli3eeee4:lastd2:xsli0ee2:vsl1:Xe2:ysli2eeee4:lastd2:xsli3ee2:vsl1:Oe2:ysli4eeeee4:lastd2:xsli4ee2:vsl1:Oe2:ysli0eeee4:lastd2:xsli4ee2:vsl1:Xe2:ysli2eeeee4:lastd2:xsli0ee2:vsl1:Xe2:ysli1eeee4:lastd2:xsli3ee2:vsl1:Oe2:ysli0eeee"
+message = "d4:lastd2:xsli2ee2:vsl1:Oe2:ysli0eee4:prevd4:prevd4:prevd4:prevd4:prevd4:lastd2:xsli1ee2:vsl1:Xe2:ysli1eee4:prevd4:lastd2:xsli1ee2:vsl1:Oe2:ysli0eee4:prevd4:lastd2:xsli4ee2:vsl1:Xe2:ysli1eee4:prevd4:prevd4:prevd4:prevd4:prevd4:prevd4:prevd4:lastd2:xsli3ee2:vsl1:Oe2:ysli1eee4:prevd4:prevd4:prevd4:lastd2:xsli0ee2:vsl1:Xe2:ysli4eee4:prevd4:lastd2:xsli4ee2:vsl1:Oe2:ysli3eee4:prevd4:lastd2:xsli3ee2:vsl1:Xe2:ysli3eee4:prevd4:lastd2:xsli2ee2:vsl1:Oe2:ysli1eee4:prevd4:lastd2:xsli3ee2:vsl1:Xe2:ysli0eee4:prevd4:prevd4:prevd4:lastd2:xsli4ee2:vsl1:Oe2:ysli0eee4:prevd4:lastd2:xsli4ee2:vsl1:Xe2:ysli0eeeee4:lastd2:xsli3ee2:vsl1:Xe2:ysli4eeee4:lastd2:xsli0ee2:vsl1:Oe2:ysli1eeeeeeeee4:lastd2:xsli0ee2:vsl1:Oe2:ysli0eeee4:lastd2:xsli2ee2:vsl1:Xe2:ysli4eeeee4:lastd2:xsli1ee2:vsl1:Xe2:ysli2eeee4:lastd2:xsli0ee2:vsl1:Oe2:ysli3eeee4:lastd2:xsli1ee2:vsl1:Xe2:ysli3eeee4:lastd2:xsli3ee2:vsl1:Oe2:ysli2eeee4:lastd2:xsli0ee2:vsl1:Xe2:ysli2eeee4:lastd2:xsli2ee2:vsl1:Oe2:ysli3eeee\"eee4:lastd2:xsli2ee2:vsl1:Oe2:ysli2eeee4:lastd2:xsli4ee2:vsl1:Xe2:ysli4eeee4:lastd2:xsli1ee2:vsl1:Oe2:ysli4eeee4:lastd2:xsli4ee2:vsl1:Xe2:ysli2eeeee"
 
 message' :: String
-message' = "d4:prevd4:prevd4:lastd2:xsli3ee2:vsl1:Oe2:ysli3eee4:prevd4:prevd4:prevd4:lastd2:xsli4ee2:vsl1:Xe2:ysli3eee4:prevd4:prevd4:prevd4:prevd4:lastd2:xsli1ee2:vsl1:Xe2:ysli0eee4:prevd4:prevd4:prevd4:lastd2:xsli0ee2:vsl1:Oe2:ysli0eee4:prevd4:prevd4:prevd4:lastd2:xsli1ee2:vsl1:Xe2:ysli1eee4:prevd4:lastd2:xsli2ee2:vsl1:Oe2:ysli2eee4:prevd4:prevd4:prevd4:prevd4:lastd2:xsli2ee2:vsl1:Oe2:ysli4eee4:prevd4:prevd4:lastd2:xsli3ee2:vsl1:Oe2:ysli1eee4:prevd4:prevd4:lastd2:xsli2ee2:vsl1:Oe2:ysli1eee4:prevd4:lastd2:xsli2ee2:vsl1:Xe2:ysli1eeeee4:lastd2:xsli0ee2:vsl1:Xe2:ysli3eeeee4:lastd2:xsli1ee2:vsl1:Xe2:ysli4eeeee4:lastd2:xsli1ee2:vsl1:Xe2:ysli2eeee4:lastd2:xsli4ee2:vsl1:Oe2:ysli1eeee4:lastd2:xsli0ee2:vsl1:Xe2:ysli4eeeeee4:lastd2:xsli4ee2:vsl1:Oe2:ysli4eeee4:lastd2:xsli2ee2:vsl1:Xe2:ysli0eeeee4:lastd2:xsli2ee2:vsl1:Xe2:ysli3eeee4:lastd2:xsli3ee2:vsl1:Oe2:ysli2eeeee4:lastd2:xsli1ee2:vsl1:Oe2:ysli3eeee4:lastd2:xsli0ee2:vsl1:Xe2:ysli2eeee4:lastd2:xsli3ee2:vsl1:Oe2:ysli4eeeee4:lastd2:xsli4ee2:vsl1:Oe2:ysli0eeee4:lastd2:xsli4ee2:vsl1:Xe2:ysli2eeeee4:lastd2:xsli0ee2:vsl1:Xe2:ysli1eeee4:lastd2:xsli3ee2:vsl1:Oe2:ysli0eeee"
+message' = "d4:lastd2:xsli2ee2:vsl1:Oe2:ysli0eee4:prevd4:prevd4:prevd4:prevd4:prevd4:lastd2:xsli1ee2:vsl1:Xe2:ysli1eee4:prevd4:lastd2:xsli1ee2:vsl1:Oe2:ysli0eee4:prevd4:lastd2:xsli4ee2:vsl1:Xe2:ysli1eee4:prevd4:prevd4:prevd4:prevd4:prevd4:prevd4:prevd4:lastd2:xsli3ee2:vsl1:Oe2:ysli1eee4:prevd4:prevd4:prevd4:lastd2:xsli0ee2:vsl1:Xe2:ysli4eee4:prevd4:lastd2:xsli4ee2:vsl1:Oe2:ysli3eee4:prevd4:lastd2:xsli3ee2:vsl1:Xe2:ysli3eee4:prevd4:lastd2:xsli2ee2:vsl1:Oe2:ysli1eee4:prevd4:lastd2:xsli3ee2:vsl1:Xe2:ysli0eee4:prevd4:prevd4:prevd4:lastd2:xsli4ee2:vsl1:Oe2:ysli0eee4:prevd4:lastd2:xsli4ee2:vsl1:Xe2:ysli0eeeee4:lastd2:xsli3ee2:vsl1:Xe2:ysli4eeee4:lastd2:xsli0ee2:vsl1:Oe2:ysli1eeeeeeeee4:lastd2:xsli0ee2:vsl1:Oe2:ysli0eeee4:lastd2:xsli2ee2:vsl1:Xe2:ysli4eeeee4:lastd2:xsli1ee2:vsl1:Xe2:ysli2eeee4:lastd2:xsli0ee2:vsl1:Oe2:ysli3eeee4:lastd2:xsli1ee2:vsl1:Xe2:ysli3eeee4:lastd2:xsli3ee2:vsl1:Oe2:ysli2eeee4:lastd2:xsli0ee2:vsl1:Xe2:ysli2eeee4:lastd2:xsli2ee2:vsl1:Oe2:ysli3eeeeeee4:lastd2:xsli2ee2:vsl1:Oe2:ysli2eeee4:lastd2:xsli4ee2:vsl1:Xe2:ysli4eeee4:lastd2:xsli1ee2:vsl1:Oe2:ysli4eeee4:lastd2:xsli4ee2:vsl1:Xe2:ysli2eeeee"
 
 type To = [[(Int, Char)]]
 
 expectedParse :: Either String JsonLikeValue
-expectedParse = Left "Error at position 247. But, dear students, please make your error more informative"
+expectedParse = Left "Error at position 965. But, dear students, please make your error more informative"
 
 expectedConvert :: Either InvalidState To
 expectedConvert = Left Duplicates
